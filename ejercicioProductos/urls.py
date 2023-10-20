2ae365700ec034aac0564fa27e609f457e69f694
LLennd <dlagos497@gmail.com>
2023-10-20 11:10:55 -0300

    vista anclada

--- ejercicioProductos/urls.py
+++ ejercicioProductos/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 from productosApp.views import *
+from app2.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",index),
